a0e5db92b01a83cfc8464983827c869623a40f39
iamshaveenvf <shaveenfernando1@gmail.com>
2023-04-09 10:48:36 +0530

    changes

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import "./Header.css";
 import PersonIcon from '@mui/icons-material/Person';
+import {IconButton}  from '@mui/material';
+import tinder from './tinder.png'
+import ForumIcon from '@mui/icons-material/Forum'
 
 function Header() {
   return (
     <div className='header'>
-      <h2>I am header</h2>
-      <PersonIcon />
+    <IconButton>
+      <PersonIcon fontSize='large' className='header__icon' />
+    </IconButton>
+
+    <img className = 'header__logo' src={tinder} alt='tinder-logo'/>
+
+    <IconButton>
+      <ForumIcon fontSize='large' className='header__icon'/>
+    </IconButton>
+    
+
     </div>
   )
 }
